d2b7d602d79bf81b9e621822709671c39c42e496
Daniel Salinas <imsplitbit@gmail.com>
2015-06-23 10:38:49 -0500

    begin breaking things out into a reusable method

--- ec2.go
+++ ec2.go
@@ -12,37 +12,51 @@ const APP_KEY string = "Application"
 const ENV_KEY string = "Environment"
 
 type EC2Instance struct {
-	id string
-	stackId string
-	ipAddress string
+	id *string
+	stackId *string
+	ipAddress *string
 }
 
 type CloudFormation struct {
-	EC2Instances []EC2Instance
-	name string
+	instances []EC2Instance
+	name *string
 }
 
 func instances(app *string, env *string) {
+	var stacks = make(map[string]CloudFormation)
+	/*
+	Make a new connection to AWS' EC2 API
+	 */
 	svc := ec2.New(&aws.Config{Region: "us-east-1"})
 
+	/*
+	Get a listing of all instances
+	 */
 	resp, err := svc.DescribeInstances(nil)
 	if err != nil {
 		panic(err)
 	}
 
+	/*
+	There are multiple reservations in the response so we must iterate
+	through those.
+	 */
 	for idx, _ := range resp.Reservations {
 		for _, inst := range resp.Reservations[idx].Instances {
+			var instance EC2Instance
+			var stackId *string
+			var ip *string
+
 			match := make(map[string]bool)
 			match["app"] = false
 			match["env"] = false
-			var stack_id *string
+
 			for _, element := range inst.Tags {
 				/*
 				iterate through all the tags to find what we're looking for
 				It's totally possible that there are no tags so I have to be
 				super defensive here.
 				 */
-//				fmt.Println(*element.Key, ": ", *element.Value)
 				if *element.Key == APP_KEY && *element.Value == *app {
 					match["app"] = true
 				} else if *element.Key == ENV_KEY && *element.Value == *env {
@@ -54,44 +68,67 @@ func instances(app *string, env *string) {
 				grab some that we care about for later
 				 */
 				if *element.Key == STACK_ID_KEY {
-					stack_id = element.Value
+					stackId = element.Value
 				}
 
 			}
-			var ip string
+
+			/*
+			If the instances has a private ip address, use that.
+			If not, use the public instead.
+			 */
 			if match["app"] && match["env"] {
 				if inst.PrivateIPAddress != nil {
-					ip = *inst.PrivateIPAddress
+					ip = inst.PrivateIPAddress
 				} else {
 					if inst.PublicIPAddress != nil {
-						ip = *inst.PublicIPAddress
+						ip = inst.PublicIPAddress
 					}
 				}
 			}
 
-			if ip != "" {
-				fmt.Println("Instance: ", *inst.InstanceID)
-				if stack_id != nil {
-					fmt.Println("Stack-ID: ", *stack_id)
+			/*
+			If there is no ip for the instance we can't get to it
+			anyway so don't print it.
+			 */
+			if *ip != "" {
+				instance.id = inst.InstanceID
+
+				if stackId != nil {
+					instance.stackId = stackId
 				}
-				fmt.Println("\tIP: ", ip)
+				instance.ipAddress = ip
+				if value, isPresent := stacks[*stackId]; !isPresent {
+					var stack CloudFormation
+					stacks[*stackId] = stack
+				}
+				var instances []EC2Instance
+				instances = stacks[*stackId].instances
+				instances = append(instances, instance)
+				stacks[*stackId].instances = instances
+				instancePrinter(&instance)
 			}
 		}
 	}
 }
 
-func printer(instance_data *sting)
+func instancePrinter(instance_data *EC2Instance) {
+	fmt.Println("Instance: ", instance_data.id)
+	fmt.Println("Stack-ID: ", instance_data.stackId)
+	fmt.Println("\tIP: ", instance_data.ipAddress)
+	fmt.Println()
+}
 
 func main() {
 	appPtr := flag.String("app", "", "The app to query in tags")
 	envPtr := flag.String("env", "", "The env to query in tags")
 	flag.Parse()
 
-	if appPtr == nil {
+	if appPtr == nil || *appPtr == "" {
 		panic("You must pass an app name with -app")
 	}
 
-	if envPtr == nil {
+	if envPtr == nil || *envPtr == "" {
 		panic("You must pass an env name with -env")
 	}
 
